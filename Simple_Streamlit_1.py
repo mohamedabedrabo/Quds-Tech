f69c12debc8b28a602a2484ba99597edab046da1
Mohamed Abedrabo <102653208+mohamedabedrabo@users.noreply.github.com>
2023-01-25 13:40:11 +0200

    Add files via upload

---
+++ Simple_Streamlit_1.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## 1. Import Libraries:
+
+# In[1]:
+
+
+import streamlit as st
+from streamlit_option_menu import option_menu
+import numpy as np
+import pandas as pd
+
+
+# ## 2. Create Sidebar:
+
+# In[2]:
+
+
+with st.sidebar:
+    selected = option_menu('Calculation App',
+                           ['Addition',
+                            'Subtraction'],
+                           
+                           icons = ['file-plus','file-minus' ],
+                           
+                           default_index = 0)
+
+
+# ## 3. Addition Page:
+
+# In[5]:
+
+
+if (selected == 'Addition'):
+    
+    # Page title:
+    st.title('Addition')
+    
+    # Getting input data from the user :
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        first = st.number_input('First Number')
+    with col2:
+        second = st.number_input('Second Number')
+
+    
+    calculation = 0
+    # Prediction Button:
+    if st.button('Calculate'):
+        calculation = first + second
+
+    st.success(calculation)
+
+
+# ## 4. Subtraction Page:
+
+# In[6]:
+
+
+if (selected == 'Subtraction'):
+    
+    # Page title:
+    st.title('Subtraction')
+    
+    # Getting input data from the user :
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        first = st.number_input('First Number')
+    with col2:
+        second = st.number_input('Second Number')
+
+    
+    calculation = 0
+    # Prediction Button:
+    if st.button('Calculate'):
+        calculation = first - second
+
+    st.success(calculation)
+
+
+# In[ ]:
+
+
+
+
